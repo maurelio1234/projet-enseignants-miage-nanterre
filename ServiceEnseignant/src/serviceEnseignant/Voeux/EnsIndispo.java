51254b311a1d4f98f3092ae59a661009b63b8d03
adelevoir <adelevoir@gmail.com>
2012-06-20 15:08:43 +0000

    MODIFICATION DU 20/06/2012 -- problème d'indispo régulière mensuelle

--- ServiceEnseignant/src/serviceEnseignant/Voeux/EnsIndispo.java
+++ ServiceEnseignant/src/serviceEnseignant/Voeux/EnsIndispo.java
@@ -8,11 +8,13 @@ import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 
 import beans.*;
 import serviceEnseignant.*;
-import sun.util.calendar.BaseCalendar.Date;
 
 
 public class EnsIndispo {
@@ -20,94 +22,233 @@ public class EnsIndispo {
 	
 	public EnsIndispo(){
 		
-		
 	}
 	
 	public void loadBD(){
 		/** Chargement du driver JDBC - Étape 1 */
 		try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 		}
 		catch( Exception ex ) {
-		System.err.println( "Erreur lors du chargement du driver" );
-		System.exit(1);
+			System.err.println( "Erreur lors du chargement du driver" );
+			System.exit(1);
 		}
 	}
 	
-	public void ajoutIndispoReg(String debut, /*String fin,*/ int dj, int poids, Enseignant ensei, int regulier, int nbO, int j) throws ParseException{
+	private Set<Date> getIndispo(Date debut, int regulier, int nbO, int j) {
+		Set<Date> ret = new HashSet<Date>();
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+	
+		GregorianCalendar calendar = new java.util.GregorianCalendar(); 
+		calendar.setTime(debut);
 		
+		System.out.println("je suis dans le getindispo et regulier : " + regulier);
+		
+		if (regulier == 1) { //HEBDO
+			///
+		} else if (regulier == 2){ // MENS
+			
+			System.out.println("je suis mensuellement dans le getindispo");
+			int semaine = calendar.get(Calendar.WEEK_OF_MONTH);
+			int mois = calendar.get(Calendar.MONTH);
+			int annee = calendar.get(Calendar.YEAR);
+			for(int i=0;i<nbO;++i) {
+				Date date = getDate(semaine, j, annee, mois+i);
+				ret.add(date);
+				
+				System.out.println("je suis mensuellement dans le getindispo BOUCLE FOR");
+			}
+		}
+		return ret;
+	}
+	
+	private Date getDate(int semaine, int j, int annee, int mois) {
+		GregorianCalendar calendar = new java.util.GregorianCalendar(annee, mois-1, 1);
+		while(semaine > 0) {
+			System.out.println("je suis dans le WHILE du getDate");
+			if (calendar.get(GregorianCalendar.DAY_OF_WEEK) == j) {				
+				semaine --;
+			}
+			calendar.add(GregorianCalendar.DAY_OF_MONTH, 1);
+		}
+		
+		java.util.Date d = calendar.getTime();
+		
+		return d;
+	}
+		
+	public void ajoutIndispoReg(String debut, /*String fin,*/ int dj, int poids, Enseignant ensei, int regulier, int nbO, int j) throws ParseException{
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		java.util.Date deb = format.parse(debut);
+		
+		System.out.println("je suis dans ajoutREG" + regulier);
+		
+		Set<Date> collectionIndispo = new HashSet<Date>();
+		collectionIndispo=getIndispo(deb, regulier, nbO, j);
+		
+		for(Date ind : collectionIndispo) {
+			System.out.println("je suis dans le DEBUT DU FOR de ajoutIndispoREG");
+			ajoutIndispoSimple(ind, poids, ensei, dj); //ajouter juste une ligne dans la table indispo
+			System.out.println("je suis dans le FOR de ajoutIndispoREG");
+		}
+	}
+	
+	public void ajoutIndispoSimple(Date debut,int poids, Enseignant ensei, int dj){
 		try {
+			System.out.println("je suis dans ajoutSIMPLE" + debut);
 			/** Connection à la base - Étape 2 */
-			String url = "jdbc:oracle:thin:@miage03.dmiage.u-paris10.fr:1521:MIAGE";
-			Connection cx = DriverManager.getConnection(url, "maletell",
-					"matthieu");
+			this.loadBD();
+
+			Connection cx = DriverManager.getConnection("jdbc:oracle:thin:@miage03.dmiage.u-paris10.fr:1521:MIAGE","maletell","matthieu");
+			cx.setAutoCommit(false);
 
 			/** Création et exécution d'une requête - Étapes 3 & 4 */
 			Statement st = cx.createStatement();
-			PreparedStatement pst = cx
-					.prepareStatement("INSERT INTO Indisponibilite VALUES(?,?,?,?)");
+			PreparedStatement pst = cx.prepareStatement("INSERT INTO Indisponibilite VALUES(?,?,?,?)");
 			
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-			java.util.Date dateD = format.parse(debut);
+//			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		//	java.util.Date dateD = format.parse(debut);
 			GregorianCalendar calendar = new java.util.GregorianCalendar(); 
-			calendar.setTime(dateD);
-				
-			//si hebdomadaire : meme traitement sql mais fait plusieurs fois
-			if (regulier == 1) {
-				for(int i=0; i<nbO; i++){
-					//ici, on ajoute une indispo autant de fois que demandé par le prof
-					
-					if(calendar.get(Calendar.DAY_OF_WEEK)==j){
-						//si le jour correspond bien à la date entrée
-						
-						//on transforme le calendar en date pour l'ajouter à la table
-						java.util.Date d = calendar.getTime();
-						pst.setDate(1,(java.sql.Date) d);
-						
-						pst.setInt(2, ensei.getNumeroEnseignant());
-						pst.setInt(3, poids);
-						pst.setInt(4,dj);
-					
-						//pour passer à la semaine suivante, on ré-utilise la date au format calendar
-						calendar.add(Calendar.WEEK_OF_MONTH, 1);
-					}
-				}
-			}
+			calendar.setTime(debut);
 			
-			//si mensuel : meme traitement sql mais fait plusieurs fois
-			if (regulier == 2) {
+			java.sql.Date sqlDate = new java.sql.Date(calendar.getTimeInMillis());
 
-				for(int i=0; i<nbO; i++){
-					//ici, on ajoute une indispo autant de fois que demandé par le prof
-					
-					if(calendar.get(Calendar.DAY_OF_WEEK)==j){
-						//si le jour correspond bien à la date entrée
-						
-						//on transforme le calendar en date pour l'ajouter à la table
-						java.util.Date d = calendar.getTime();
-						pst.setDate(1,(java.sql.Date) d);
-						
-						pst.setInt(2, ensei.getNumeroEnseignant());
-						pst.setInt(3, poids);
-						pst.setInt(4,dj);
-					
-	// VERIFIER QUE AVANCE BIEN D'UN LUNDI A UN AUTRE : CF PB DES MOIS DE 28/29/30/31 JOURS
-						
-						//pour passer au mois suivant, on ré-utilise la date au format calendar
-						calendar.add(Calendar.MONTH, 1);
-						System.out.println("mois avant : "+ d + " mois apr : " + calendar);
-					}
-				}
-			}
-			/** Fermetures - Étape 6 */
+			pst.setDate(1,sqlDate);
+			pst.setInt(2, ensei.getNumeroEnseignant());
+			pst.setInt(3, poids);
+			pst.setInt(4,dj);
+			
+			pst.executeUpdate();
+			System.out.println("je suis apres le executeUpdate du ajoutIndispoSIMPLE");
+			cx.commit();
+			
 			pst.close();
 			st.close();
 			cx.close();
-		} catch (SQLException ex) {
+		}catch (SQLException ex) {
+			ex.printStackTrace();
 			System.err.println("Erreur lors de la cx à la base");
 			System.exit(1);
 		}
-    }
+	}	
+//		try {
+//			System.out.println("coucou!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//			/** Connection à la base - Étape 2 */
+//			this.loadBD();
+//
+//			Connection cx = DriverManager.getConnection("jdbc:oracle:thin:@miage03.dmiage.u-paris10.fr:1521:MIAGE","maletell","matthieu");
+//			System.out.println("typeeeeeeeeee 222522222222 " + regulier);
+//cx.setAutoCommit(false);
+//
+//			/** Création et exécution d'une requête - Étapes 3 & 4 */
+//			Statement st = cx.createStatement();
+//			PreparedStatement pst = cx
+//					.prepareStatement("INSERT INTO Indisponibilite VALUES(?,?,?,?)");
+//			
+//			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//			java.util.Date dateD = format.parse(debut);
+//			GregorianCalendar calendar = new java.util.GregorianCalendar(); 
+//			calendar.setTime(dateD);
+//			
+//			GregorianCalendar calendar2 = new java.util.GregorianCalendar();
+//			
+//			System.out.println("type : "+ regulier);
+//				
+//			//si hebdomadaire : meme traitement sql mais fait plusieurs fois
+//			if (regulier == 1) {
+//				for(int i=0; i<nbO; i++){
+//					//ici, on ajoute une indispo autant de fois que demandé par le prof
+//					
+//					if(calendar.get(Calendar.DAY_OF_WEEK)==j){
+//						//si le jour correspond bien à la date entrée
+//						
+//						//on transforme le calendar en date pour l'ajouter à la table
+//						//java.util.Date d = calendar.getTime();
+//						java.sql.Date sqlDate = new java.sql.Date(calendar.getTimeInMillis());
+//						System.out.println("hi");
+//						pst.setDate(1,sqlDate);
+//
+//						
+//						pst.setInt(2, ensei.getNumeroEnseignant());
+//						pst.setInt(3, poids);
+//						pst.setInt(4,dj);
+//						System.out.println("test ap SET de hebdo");
+//						pst.executeUpdate();
+//						
+//						cx.commit();
+//					System.out.println("test ap executeUpdate de hebdo");
+//					
+//						//pour passer à la semaine suivante, on ré-utilise la date au format calendar
+//						calendar.add(Calendar.WEEK_OF_MONTH, 1);
+//						
+//						
+//						
+//					}
+//				}
+//				
+//			}
+//			
+//			//si mensuel : meme traitement sql mais fait plusieurs fois
+//			if (regulier == 2) {
+//
+//				for(int i=0; i<nbO; i++){
+//					//ici, on ajoute une indispo autant de fois que demandé par le prof
+//					
+//					if(calendar.get(Calendar.DAY_OF_WEEK)==j){
+//						//si le jour correspond bien à la date entrée
+//						
+//						//on transforme le calendar en date pour l'ajouter à la table
+//						java.sql.Date sqlDate = new java.sql.Date(calendar.getTimeInMillis());
+//						pst.setDate(1,sqlDate);
+//						
+//						pst.setInt(2, ensei.getNumeroEnseignant());
+//						pst.setInt(3, poids);
+//						pst.setInt(4,dj);
+//						System.out.println("test ap SET de mens");
+//						pst.executeUpdate();
+//						cx.commit();
+//						System.out.println("test ap executeUpdate de mens");
+//	// VERIFIER QUE AVANCE BIEN D'UN LUNDI A UN AUTRE : CF PB DES MOIS DE 28/29/30/31 JOURS
+//						
+//						
+//						calendar2 = calendar;
+//						
+//						//pour passer au mois suivant, on ré-utilise la date au format calendar
+//						calendar.add(Calendar.MONTH, 1);
+//						
+//						
+//						while(calendar.get(Calendar.DAY_OF_WEEK)!= j ){
+//							
+//							System.out.println("Je SUIS DANS CETTE SATANEE BOUCLE DE ****TUUUUUUUUUUUUUT*******");
+//							
+//							if(calendar.get(Calendar.DAY_OF_WEEK)>j){
+//								calendar.add(Calendar.DAY_OF_MONTH, -1);
+//							}
+//							if(calendar.get(Calendar.DAY_OF_WEEK)<j){
+//								calendar.add(Calendar.DAY_OF_MONTH, 1);
+//							}
+//						}
+//						
+//						System.out.println(calendar);
+//						System.out.println("mois avant : "+ calendar + " mois apr : " + calendar);
+//						
+//						//calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar2.get(Calendar.WEEK_OF_MONTH));
+//						System.out.println("\n\n nouvelle date : " + calendar);
+//					}
+//				}
+//			}
+//			/** Fermetures - Étape 6 */
+//		
+//		System.out.println("teeeeeeeeeest");
+//			pst.close();
+//			st.close();
+//			cx.close();
+//		} catch (SQLException ex) {
+//			ex.printStackTrace();
+//			System.err.println("Erreur lors de la cx à la base");
+//			System.exit(1);
+//		}
+//    }
 	
 	
 	
