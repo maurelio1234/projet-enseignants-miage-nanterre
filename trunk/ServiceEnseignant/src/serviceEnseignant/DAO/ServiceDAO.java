a2caf2343986ed45b930cfff555e4c320079fd50
wongemilie@gmail.com <wongemilie@gmail.com@0d1bbc46-05d9-69e2-4250-36ff7cd34926>
2012-06-22 11:56:08 +0000

    nouvelle DAO Service

---
+++ trunk/ServiceEnseignant/src/serviceEnseignant/DAO/ServiceDAO.java
@@ -0,0 +1,36 @@
+package serviceEnseignant.DAO;
+
+import beans.Service;
+
+public class ServiceDAO extends DAO<Service>{
+
+	public static String TABLE = "SERVICE";
+	
+	@Override
+	public Service find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Service create(Service obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Service update(Service obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Service obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+}
