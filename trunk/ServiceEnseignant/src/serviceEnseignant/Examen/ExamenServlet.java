76ee43e6e05827c81fe63dad72468791dd23aa43
letellier.matthieu@gmail.com <letellier.matthieu@gmail.com@0d1bbc46-05d9-69e2-4250-36ff7cd34926>
2012-06-24 23:01:24 +0000

    Ajout de la servlet UpdateExamenServlet (maj d'un examen). Initialisation possible via la BDD Code non testé en raison de l'absence de certaines DAO.

--- trunk/ServiceEnseignant/src/serviceEnseignant/Examen/ExamenServlet.java
+++ trunk/ServiceEnseignant/src/serviceEnseignant/Examen/ExamenServlet.java
@@ -9,7 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import serviceEnseignant.DAO.EnseignantDAO;
+import serviceEnseignant.DAO.ExamenDAO;
+import serviceEnseignant.DAO.NoteDAO;
 import beans.*;
+
 /**
  * Servlet implementation class ExamenServlet
  */
@@ -17,34 +22,68 @@ import beans.*;
 public class ExamenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static Examen exam;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExamenServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public ExamenServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	System.out.println("Création des candidats");
-		Candidat c1 = new Candidat(1, "Letellier", "Matthieu", "osef", "Osef", null, "maletell", "matthieu", null, true, "osef");
-		Candidat c2 = new Candidat(2, "Bonneau", "Julie", "osef", "Osef", null, "osef", "osef", null, true, "osef");
-		Candidat c3 = new Candidat(3, "Vignau", "Morgane", "osef", "Osef", null, "osef", "osef", null, true, "osef");
-		Candidat c4 = new Candidat(4, "Levoir", "Adelaide", "osef", "Osef", null, "osef", "osef", null, true, "osef");
-		Candidat c5 = new Candidat(5, "Wong", "Emilie", "osef", "Osef", null, "osef", "osef", null, true, "osef");
-		Candidat c6 = new Candidat(6, "Ravelo", "Heni", "osef", "Osef", null, "osef", "osef", null, true, "osef");
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		//Choix de l'initialisation via la BDD ou via la méthode "en dur".
+		boolean SimpleInit = false;
 		
+		Examen exam;
+		ExamenDAO examDAO = new ExamenDAO();
+		NoteDAO noteDAO = new NoteDAO();
+
+
+		if (SimpleInit) {
+			exam = ExamenServlet.InitTest();
+		} else {
+			int num_examen = 1;
+			// A initialiser via un num_Examen récupéré.
+			exam = examDAO.find(num_examen);
+			exam.setMesNotes(examDAO.LoadNote(exam));
+		}
+		request.setAttribute("ExamBeans", exam);
+		RequestDispatcher disp = getServletContext().getRequestDispatcher(
+				"/jsp/jspExamen/ConsulterExamen.jsp");
+		disp.forward(request, response);
+
+	}
+
+	private static Examen InitTest() {
+		System.out.println("Création des candidats");
+		Candidat c1 = new Candidat(1, "Letellier", "Matthieu", "osef", "Osef",
+				null, "maletell", "matthieu", null, true, "osef");
+		Candidat c2 = new Candidat(2, "Bonneau", "Julie", "osef", "Osef", null,
+				"osef", "osef", null, true, "osef");
+		Candidat c3 = new Candidat(3, "Vignau", "Morgane", "osef", "Osef",
+				null, "osef", "osef", null, true, "osef");
+		Candidat c4 = new Candidat(4, "Levoir", "Adelaide", "osef", "Osef",
+				null, "osef", "osef", null, true, "osef");
+		Candidat c5 = new Candidat(5, "Wong", "Emilie", "osef", "Osef", null,
+				"osef", "osef", null, true, "osef");
+		Candidat c6 = new Candidat(6, "Ravelo", "Heni", "osef", "Osef", null,
+				"osef", "osef", null, true, "osef");
+
 		System.out.println("\nCréation des Etudiants.");
 		Etudiant e1 = new Etudiant(1);
 		e1.setMonCandidat(c1);
@@ -58,25 +97,28 @@ public class ExamenServlet extends HttpServlet {
 		e5.setMonCandidat(c5);
 		Etudiant e6 = new Etudiant(6);
 		e6.setMonCandidat(c6);
-		
-		System.out.println("\nCréation de l'examen");
-		exam = new Examen(1,"10:30","Test",1);
-		System.out.println("\nCréation des notes");
-		Note n1 = new Note(e1,exam,-2);
-		Note n2 = new Note(e2,exam,10);
-		Note n3 = new Note(e3,exam,20);
-		Note n4 = new Note(e4,exam,0);
-		Note n5 = new Note(e5,exam,-1);
-		Note n6 = new Note(e6,exam,-3);
 
-		for(int i = 0;i < exam.getMesNotes().size();i++){
-			System.out.println(exam.getMesNotes().get(i).getMonEtudiant().getMonCandidat().getNom() + " " + exam.getMesNotes().get(i).getMonEtudiant().getMonCandidat().getPrenom() + " " + exam.getMesNotes().get(i).getNote());
+		System.out.println("\nCréation de l'examen");
+		exam = new Examen(1, "10:30", "Test", 1);
+		System.out.println("\nCréation des notes");
+		Note n1 = new Note(e1, exam, -2);
+		Note n2 = new Note(e2, exam, 10);
+		Note n3 = new Note(e3, exam, 20);
+		Note n4 = new Note(e4, exam, 0);
+		Note n5 = new Note(e5, exam, -1);
+		Note n6 = new Note(e6, exam, -3);
+
+		for (int i = 0; i < exam.getMesNotes().size(); i++) {
+			System.out.println(exam.getMesNotes().get(i).getMonEtudiant()
+					.getMonCandidat().getNom()
+					+ " "
+					+ exam.getMesNotes().get(i).getMonEtudiant()
+							.getMonCandidat().getPrenom()
+					+ " "
+					+ exam.getMesNotes().get(i).getNote());
 		}
 		System.out.println("\nFin de l'initialisation.");
-			request.setAttribute("ExamBeans", exam);
-			RequestDispatcher disp=	getServletContext().getRequestDispatcher("/jsp/jspExamen/ConsulterExamen.jsp");
-			disp.forward(request, response);
-
+		return exam;
 	}
 
 }
---
+++ trunk/ServiceEnseignant/src/serviceEnseignant/Examen/UpdateExamenServlet.java
@@ -0,0 +1,110 @@
+package serviceEnseignant.Examen;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Etudiant;
+import beans.Examen;
+import beans.Jours;
+import beans.Note;
+
+import serviceEnseignant.DAO.ExamenDAO;
+import serviceEnseignant.DAO.NoteDAO;
+
+/**
+ * Servlet implementation class UpdateExamenServlet
+ */
+@WebServlet("/UpdateExamenServlet")
+public class UpdateExamenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateExamenServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public GregorianCalendar ConvertirDate(String date){
+		String delims = "[/]";
+		String[] tokens = date.split(delims);
+		GregorianCalendar gDate = null;
+		if (tokens.length == 3) {
+			int jour = Integer.parseInt(tokens[0]);
+			int mois = Integer.parseInt(tokens[1]) -1;
+			int annee = Integer.parseInt(tokens[2]);
+			try {
+				gDate = new GregorianCalendar(annee, mois, jour);
+				gDate.setLenient(false);
+			} catch (Exception e) {
+				System.out.println("Le Format de la date n'est pas valide");
+				return null;
+			}
+		}
+		return gDate;
+	}
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TO_DO : dans ConsulterExamen. Comment gérer le -1 ? (non visible à la saisie, mais doit pouvoir être récupéré ...)
+		//Mise à jour des informations de l'examen : Libelle, Date, Horaire, Coefficent.
+		ExamenDAO examDAO = new ExamenDAO();
+		EtudiantDAO etudiantDAO = new EtudiantDAO();
+		NoteDAO noteDAO = new NoteDAO();
+		
+		PrintWriter out = response.getWriter(); 
+		
+		Integer num_exam = Integer.parseInt(request.getParameter("num_exam"));
+		String libelle = request.getParameter("libelle");
+		String date = request.getParameter("date");
+		String horaire = request.getParameter("horaire");
+		Double coeff = Double.parseDouble(request.getParameter("coeff"));
+		
+		Examen exam = examDAO.find(num_exam);
+		exam.setCoefficient(coeff);
+		exam.setDate(new Jours(ConvertirDate(date)));
+		exam.setHoraire(horaire);
+		exam.setLibelle(libelle);
+		
+		examDAO.update(exam);
+		
+		// Mise à jour des notes
+		List<Note> listeNotes = new ArrayList<Note>();
+		Etudiant etudiant;
+		Note note;
+		for (int i = 0; i < Integer.parseInt(request.getParameter("nb_notes")); i++){
+			exam = examDAO.find(Integer.parseInt(request.getParameter("RefExamen"+i)));
+			etudiant = etudiantDAO.find(Integer.parseInt(request.getParameter("RefEtudiant"+i)));
+			switch(Integer.parseInt(request.getParameter("choix"+i))){
+			case 1 : note = new Note(etudiant, exam, Double.parseDouble(request.getParameter("Note"+i))); break;
+			case 2 : note = new Note(etudiant, exam, -2); break;
+			case 3 : note = new Note(etudiant, exam, -3); break;
+			}
+			
+			noteDAO.update(note);
+		}
+		
+		// Renvoi vers la page d'affichage des notes.
+		RequestDispatcher disp=	getServletContext().getRequestDispatcher("/ConsulterExamen.jsp");
+		disp.forward(request, response);
+	}
+
+}
