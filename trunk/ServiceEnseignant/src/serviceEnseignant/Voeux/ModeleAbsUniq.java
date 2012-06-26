5773e9489826c141dbbe5c9e58c51c3dc546f968
adelevoir@gmail.com <adelevoir@gmail.com@0d1bbc46-05d9-69e2-4250-36ff7cd34926>
2012-06-26 12:41:55 +0000

    Ajout de contrôles et affichage des erreurs associées

--- trunk/ServiceEnseignant/src/serviceEnseignant/Voeux/ModeleAbsUniq.java
+++ trunk/ServiceEnseignant/src/serviceEnseignant/Voeux/ModeleAbsUniq.java
@@ -2,7 +2,12 @@ package serviceEnseignant.Voeux;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,37 +21,106 @@ import javax.servlet.http.HttpServletResponse;
 public class ModeleAbsUniq extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private EnsIndispo enseignant;
-	private int ref=2;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private int ref = 2;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
 	public ModeleAbsUniq() {
 		super();
 		enseignant = new EnsIndispo();
 	}
 
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-System.out.println(request.getParameter("poids"));
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		RequestDispatcher dispatch = this.getServletContext()
+				.getRequestDispatcher("/jsp/jspVoeux/index.jsp");
+
 		String dD = request.getParameter("dateDebut");
 		String dF = request.getParameter("dateFin");
 		String prio = request.getParameter("poids");
-		
-		int priorite = Integer.parseInt(prio);
-		
+		String dur = request.getParameter("demiJ");
+
+		String valNulle = "";
+
 		try {
-			enseignant.ajoutIndispoUniq(dD,dF,priorite,ref);
+
+			// si toutes les valeurs sont renseignées
+			if (!dD.equalsIgnoreCase(valNulle)
+					&& !dF.equalsIgnoreCase(valNulle)
+					&& !prio.equalsIgnoreCase(valNulle)
+					&& !dur.equalsIgnoreCase(valNulle)) {
+
+				System.out.println("dans le premier IF de modeleAbsUniq");
+
+				System.out.println("modeleabsunique");
+				int priorite = Integer.parseInt(prio);
+				int duree = Integer.parseInt(dur);
+
+				// transformer le format string en calendar pour comparer les
+				// dates
+				SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+				Calendar d = format.getCalendar();
+
+				// date de debut
+				Date dateDbt;
+				dateDbt = format.parse(dD);
+				GregorianCalendar dateD = new GregorianCalendar();
+				dateD.setTime(dateDbt);
+
+				// date de fin
+				Date dateFin;
+				dateFin = format.parse(dF);
+				GregorianCalendar dateF = new GregorianCalendar();
+				dateF.setTime(dateFin);
+
+				// si la date entrée est bien postérieure à la date du jour
+				// courant
+				if (dateD.after(Calendar.getInstance().getTime())) {
+
+					if (dateD.before(dateF)) {
+						enseignant.ajoutIndispoUniq(dD, dF, priorite, ref,
+								duree);
+
+						dispatch.forward(request, response);
+
+					} else {
+						// si date début apres date de fin
+
+						request.setAttribute("erreurUniq","La date de début est postérieure à la date de fin");
+
+						dispatch.forward(request, response);
+					}
+				} else {
+					request.setAttribute("erreurUniq","La date de début est antérieure à la date d'aujourd'hui");
+
+					dispatch.forward(request, response);
+				}
+
+			} else { // si pas toutes renseignées
+
+				System.out.println("dans le deuxieme ELSE de modeleAbsUniq");
+
+				request.setAttribute("erreurUniq","ERREUR : un (ou plusieurs) paramètre n'a pas été renseigné.");
+
+				dispatch.forward(request, response);
+
+			}
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 			System.out.println("erreur ex");
